5d7ba271ea3f0d5db3d0c9bd8ed45f5dac8022c2
继能 王 <hale_wang@outlook.com>
2018-09-10 22:08:34 +0800

    Update Source code.

--- SerialAssistant/MainPage.xaml.cs
+++ SerialAssistant/MainPage.xaml.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Devices.Enumeration;
+using Windows.Devices.SerialCommunication;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
 using Windows.UI.Xaml;
@@ -27,71 +29,71 @@ namespace SerialAssistant
             this.InitializeComponent();
         }
 
-        //public async void SerialPortList()
-        //{
-        //    var selector = SerialDevice.GetDeviceSelector();
-        //    var devices = await DeviceInformation.FindAllAsync(selector);
-        //    if (devices.Any())
-        //    {
-        //        for (int i = 0; i < devices.Count(); i++)
-        //        {
-        //            PortIDComboBox.Items.Add(devices[i].Name);
-        //        }
-        //    //PortIDComboBox.SelectedIndex = 0;
+        public async void SerialPortList()
+        {
+            var selector = SerialDevice.GetDeviceSelector();
+            var devices = await DeviceInformation.FindAllAsync(selector);
+            if (devices.Any())
+            {
+                for (int i = 0; i < devices.Count(); i++)
+                {
+                    PortIDComboBox.Items.Add(devices[i].Name);
+                }
+                //PortIDComboBox.SelectedIndex = 0;
 
-        //        for (int i = 0; i < devices.Count(); i++)
-        //        {
-        //            if (devices[i].Name.Equals(PortIDComboBox.SelectedValue.ToString()))
-        //            {
-        //                SerialDevice serialDevice = await SerialDevice.FromIdAsync(devices[i].Id);
-        //                serialDevice.BaudRate = Convert.ToUInt32(BaudRateComboBox.SelectedValue);
+                for (int i = 0; i < devices.Count(); i++)
+                {
+                    if (devices[i].Name.Equals(PortIDComboBox.SelectedValue.ToString()))
+                    {
+                        SerialDevice serialDevice = await SerialDevice.FromIdAsync(devices[i].Id);
+                        serialDevice.BaudRate = Convert.ToUInt32(BaudRateComboBox.SelectedValue);
 
-        //                //无校验（no parity）
-        //                //奇校验（odd parity）：如果字符数据位中"1"的数目是偶数，校验位为"1"，如果"1"的数目是奇数，校验位应为"0"。（校验位调整个数）
-        //                //偶校验（even parity）：如果字符数据位中"1"的数目是偶数，则校验位应为"0"，如果是奇数则为"1"。（校验位调整个数）
-        //                //mark parity：校验位始终为1
-        //                //space parity：校验位始终为0
-        //                string SelectorParity = ParityBitComboBox.SelectedValue.ToString();
-        //                switch (SelectorParity)
-        //                {
-        //                    case "无校验":
-        //                        serialDevice.Parity = SerialParity.None;
-        //                        break;
-        //                    case "奇校验":
-        //                        serialDevice.Parity = SerialParity.Odd;
-        //                        break;
-        //                    case "偶校验":
-        //                        serialDevice.Parity = SerialParity.Even;
-        //                        break;
-        //                    case "校验位为1":
-        //                        serialDevice.Parity = SerialParity.Mark;
-        //                        break;
-        //                    case "校验位为0":
-        //                        serialDevice.Parity = SerialParity.Space;
-        //                        break;
-        //                }
+                        //无校验（no parity）
+                        //奇校验（odd parity）：如果字符数据位中"1"的数目是偶数，校验位为"1"，如果"1"的数目是奇数，校验位应为"0"。（校验位调整个数）
+                        //偶校验（even parity）：如果字符数据位中"1"的数目是偶数，则校验位应为"0"，如果是奇数则为"1"。（校验位调整个数）
+                        //mark parity：校验位始终为1
+                        //space parity：校验位始终为0
+                        string SelectorParity = ParityBitComboBox.SelectedValue.ToString();
+                        switch (SelectorParity)
+                        {
+                            case "无校验":
+                                serialDevice.Parity = SerialParity.None;
+                                break;
+                            case "奇校验":
+                                serialDevice.Parity = SerialParity.Odd;
+                                break;
+                            case "偶校验":
+                                serialDevice.Parity = SerialParity.Even;
+                                break;
+                            case "校验位为1":
+                                serialDevice.Parity = SerialParity.Mark;
+                                break;
+                            case "校验位为0":
+                                serialDevice.Parity = SerialParity.Space;
+                                break;
+                        }
 
-        //                serialDevice.DataBits = Convert.ToUInt16(DataBitComboBox.SelectedValue);
+                        serialDevice.DataBits = Convert.ToUInt16(DataBitComboBox.SelectedValue);
 
-        //                switch (StopBitComboBox.SelectedValue.ToString())
-        //                {
-        //                    case "1":
-        //                        serialDevice.StopBits = SerialStopBitCount.One;
-        //                        break;
-        //                    case "1.5":
-        //                        serialDevice.StopBits = SerialStopBitCount.OnePointFive;
-        //                        break;
-        //                    case "2":
-        //                        serialDevice.StopBits = SerialStopBitCount.Two;
-        //                        break;
-        //                }
-        //                SendTextBox.Text = serialDevice.PortName + ", " + serialDevice.BaudRate + ", " + serialDevice.Parity + ", " + serialDevice.DataBits + ", " + serialDevice.StopBits;
-        //                return;
-        //            }
-        //        }
+                        switch (StopBitComboBox.SelectedValue.ToString())
+                        {
+                            case "1":
+                                serialDevice.StopBits = SerialStopBitCount.One;
+                                break;
+                            case "1.5":
+                                serialDevice.StopBits = SerialStopBitCount.OnePointFive;
+                                break;
+                            case "2":
+                                serialDevice.StopBits = SerialStopBitCount.Two;
+                                break;
+                        }
+                        SendTextBox.Text = serialDevice.PortName + ", " + serialDevice.BaudRate + ", " + serialDevice.Parity + ", " + serialDevice.DataBits + ", " + serialDevice.StopBits;
+                        return;
+                    }
+                }
 
-        //    }
-        //}
+            }
+        }
 
         private void ClearReceiveButton_Click(object sender, Windows.UI.Xaml.RoutedEventArgs e)
         {
